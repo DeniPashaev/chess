9158631748b7441ff2ac4ad6db0e06483c98245a
Deni <dendi_maksimof@mail.ru>
2024-05-17 18:42:11 +0300

    end

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -1,13 +1,17 @@
-import { Colors } from '../colors';
-import { Cell } from '../Cell';
-import { FgigureNames, Figure } from "./Figures";
-import blackLogo from "../../assets/black-king.png"
-import whiteLogo from "../../assets/white-king.png"
+import { Colors } from "../Colors";
+import { Cell } from "../Cell";
+import { FgigureNames, Figure } from "./Figure";
+import blackLogo from "../../assets/black-king.png";
+import whiteLogo from "../../assets/white-king.png";
 
-export class King extends Figure{
-    constructor(color: Colors, cell: Cell) {
-        super(color, cell)
-        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo
-        this.name = FgigureNames.KING
-    }
+export class King extends Figure {
+  constructor(color: Colors, cell: Cell) {
+    super(color, cell);
+    this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+    this.name = FgigureNames.KING;
+  }
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) return false;
+    return true;
+  }
 }
